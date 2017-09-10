48ee48b05866014a063de2cbf99190e0ba7d2c49
fzc621 <fzc621@gmail.com>
2017-09-10 12:40:42 +0800

    finish tts

--- NewsApp/app/src/main/java/com/java/seven/newsapp/chinesenews/content/ContentActivity.java
+++ NewsApp/app/src/main/java/com/java/seven/newsapp/chinesenews/content/ContentActivity.java
@@ -16,6 +16,11 @@ import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.baidu.tts.auth.AuthInfo;
+import com.baidu.tts.client.SpeechError;
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
 import com.bumptech.glide.Glide;
 import com.java.seven.newsapp.R;
 import com.java.seven.newsapp.adapter.NewsSummaryAdapter;
@@ -26,7 +31,7 @@ import com.java.seven.newsapp.util.SharedPreferencesUtil;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ContentActivity extends AppCompatActivity implements ContentContract.View {
+public class ContentActivity extends AppCompatActivity implements ContentContract.View, SpeechSynthesizerListener {
 
     @Bind(R.id.toolbar_layout)
     CollapsingToolbarLayout collapsingToolbarLayout;
@@ -35,9 +40,11 @@ public class ContentActivity extends AppCompatActivity implements ContentContrac
     @Bind(R.id.news_content)
     WebView newsContent;
     private String id;
+    private String text;
 
     private FloatingActionButton fab;
     private ContentContract.Presenter presenter;
+    private SpeechSynthesizer mSpeechSynthesizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +70,6 @@ public class ContentActivity extends AppCompatActivity implements ContentContrac
 //        });
         fab.setVisibility(View.INVISIBLE);
         presenter.getNews(id);
-
     }
 
     @Override
@@ -73,6 +79,10 @@ public class ContentActivity extends AppCompatActivity implements ContentContrac
 
     @Override
     public void setContent(String text) {
+        if(text.length() < 500)
+            this.text = text;
+        else
+            this.text = text.substring(0,500);
         newsContent.loadDataWithBaseURL(null, HtmlFormat.getNewContent(text), "text/html", "utf-8", null);
         newsContent.getSettings().setJavaScriptEnabled(true);
         WebSettings settings = newsContent.getSettings();
@@ -108,10 +118,14 @@ public class ContentActivity extends AppCompatActivity implements ContentContrac
             return true;
         }*/
         switch (item.getItemId()) {
+            case R.id.item_favorite:
+                Toast.makeText(this, "favorite被选择了", Toast.LENGTH_SHORT).show();
+                break;
             case R.id.item_share:
                 Toast.makeText(this, "share被选择了", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.item_speak:
+                startTTS();
                 Toast.makeText(this, "speak被选择了", Toast.LENGTH_SHORT).show();
                 break;
             default:
@@ -120,4 +134,50 @@ public class ContentActivity extends AppCompatActivity implements ContentContrac
         return super.onOptionsItemSelected(item);
     }
 
+    private void startTTS() {
+        // 获取语音合成对象实例
+        mSpeechSynthesizer = SpeechSynthesizer.getInstance();
+        // 设置context
+        mSpeechSynthesizer.setContext(this);
+        // 设置语音合成状态监听器
+        mSpeechSynthesizer.setSpeechSynthesizerListener(this);
+
+        // 获取语音合成授权信息
+        AuthInfo authInfo = mSpeechSynthesizer.auth(TtsMode.MIX);
+        // 判断授权信息是否正确，如果正确则初始化语音合成器并开始语音合成，如果失败则做错误处理
+        if (authInfo.isSuccess()) {
+            mSpeechSynthesizer.initTts(TtsMode.MIX);
+            mSpeechSynthesizer.speak(text);
+        } else {
+            // 授权失败
+        }
+    }
+
+    public void onError(String arg0, SpeechError arg1) {
+        // 监听到出错，在此添加相关操作
+    }
+    public void onSpeechFinish(String arg0) {
+        // 监听到播放结束，在此添加相关操作
+    }
+    public void onSpeechProgressChanged(String arg0, int arg1) {
+        // 监听到播放进度有变化，在此添加相关操作
+    }
+    public void onSpeechStart(String arg0) {
+        // 监听到合成并播放开始，在此添加相关操作
+    }
+    public void onSynthesizeDataArrived(String arg0, byte[] arg1, int arg2) {
+        // 监听到有合成数据到达，在此添加相关操作
+    }
+    public void onSynthesizeFinish(String arg0) {
+        // 监听到合成结束，在此添加相关操作
+    }
+    public void onSynthesizeStart(String arg0) {
+        // 监听到合成开始，在此添加相关操作
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        mSpeechSynthesizer.stop();
+    }
 }
--- NewsApp/app/src/main/java/com/java/seven/newsapp/util/AppConstants.java
+++ NewsApp/app/src/main/java/com/java/seven/newsapp/util/AppConstants.java
@@ -6,4 +6,10 @@ package com.java.seven.newsapp.util;
 
 public class AppConstants {
     public static final String ISNIGHT = "isNight";
+    public static final String BAIDU_APIKEY = "gOEBHTnzGwy58HkcsOktcC9g";
+    public static final String BAIDU_SECRET_KEY = "1b993f3de8e6eea683567c0b46670877";
+    public static final String BAIDU_APP_ID = "10118270";
+    public static final String TTS_TEXT_FILE = "bd_etts_ch_text.dat";
+    public static final String TTS_SPEECH_FILE = "bd_etts_ch_speech_female.dat";
+    public static final String DIR_NAME = "SevenNews";
 }
